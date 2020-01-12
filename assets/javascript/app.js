d20eb2db0a9eea23d4b6f8907e40212953ceb7c1
Airmid <forsa9828@gmail.com>
2020-01-12 13:26:00 -0600

    updating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,185 @@
-// allows the document to load before js starts
 $(document).ready(function(){
-    
-  })
   
-  var trivia = {
-    // trivia properties
-    correct: 0,
-    incorrect: 0,
-    unanswered: 0,
-    currentSet: 0,
-    timerId : '',
-    // questions options and answers data
-    questions: {
-      q1: 'Jamie a McKenzie?',
-      q2: 'Claire is a widow ',
-      q3: 'Culloden Moor is a war',
-    },
-    options: {
-      q1: ['True', 'False'],
-      q2: ['True', 'False'],
-      q3: ['True', 'False'],
-    },
-    answers: {
-      q1: 'True',
-      q2: 'False',
-      q3: 'True',
-    },
-    // trivia methods
-    // method to initialize game
-    startGame: function(){
-      // restarting game results
-      trivia.currentSet = 0;
-      trivia.correct = 0;
-      trivia.incorrect = 0;
-      trivia.unanswered = 0;
+  // event listeners
+  $("#remaining-time").hide();
+  $("#start").on('click', trivia.startGame);
+  $(document).on('click' , '.option', trivia.guessChecker);
+  
+})
+
+var trivia = {
+  // trivia properties
+  correct: 0,
+  incorrect: 0,
+  unanswered: 0,
+  currentSet: 0,
+  timer: 20,
+  timerOn: false,
+  timerId : '',
+  // questions options and answers data
+  questions: {
+    q1: 'What does Jamie call Claire?',
+    q2: 'The name of Jamie and Claires firts child?',
+    q3: 'The name of The Fraser settlement in North Caroina?',
+    q4: 'What are the name of the stones that travel back in time?',
+    q5: "Is Jamie a Mackenzi?",
+    q6: 'What is the name of the castle Jamie is from?',
+    q7: "What is Claires occupation?"
+  },
+  options: {
+    q1: ['Lady Broch Tuarach', 'La Dame Blanche', 'Beauchamp', 'Sassenach'],
+    q2: ['Jamie', 'Helen', 'Faith', 'Brianna'],
+    q3: ['Frasers Ridge', 'Frasers Creek','Fraser Highlands'],
+    q4: ['Craigh na Dun', 'Dun Carloway', 'Callanish', 'Clava Cairn'],
+    q5: ['True','False'],
+    q6: ['Lallybroch','Fraser','Dunnottar','Lovat'],
+    q7: ['Nurse', 'Healer', 'La Dame Blache','All']
+  },
+  answers: {
+    q1: 'Sassenach',
+    q2: 'Faith',
+    q3: 'Frasers Ridge',
+    q4: 'Craigh na Dun',
+    q5: 'True',
+    q6: 'Lallybroch',
+    q7: 'All'
+  },
+  // trivia methods
+  // method to initialize gamea
+  startGame: function(){
+    // restarting game results
+    trivia.currentSet = 0;
+    trivia.correct = 0;
+    trivia.incorrect = 0;
+    trivia.unanswered = 0;
+    clearInterval(trivia.timerId);
+    
+    
+    // show game section
+    $('#game').show();
+    
+    //  empty last results
+    $('#results').html('');
+    
+    // show timer
+    $('#timer').text(trivia.timer);
+    
+    // remove start button
+    $('#start').hide();
+
+    $('#remaining-time').show();
+    
+    // ask first question
+    trivia.nextQuestion();
+    
+  },
+  // method to loop through and display questions and options 
+  nextQuestion : function(){
+    
+    // set timer to 20 seconds each question
+    trivia.timer = 10;
+     $('#timer').removeClass('last-seconds');
+    $('#timer').text(trivia.timer);
+    
+    // to prevent timer speed up
+    if(!trivia.timerOn){
+      trivia.timerId = setInterval(trivia.timerRunning, 1000);
+    }
+    
+    // gets all the questions then indexes the current questions
+    var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+    $('#question').text(questionContent);
+    
+    // an array of all the user options for the current question
+    var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+    
+    // creates all the trivia guess options in the html
+    $.each(questionOptions, function(index, key){
+      $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
+    })
+    
+  },
+  // method to decrement counter and count unanswered if timer runs out
+  timerRunning : function(){
+    // if timer still has time left and there are still questions left to ask
+    if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+      $('#timer').text(trivia.timer);
+      trivia.timer--;
+        if(trivia.timer === 4){
+          $('#timer').addClass('last-seconds');
+        }
+    }
+    // the time has run out and increment unanswered, run result
+    else if(trivia.timer === -1){
+      trivia.unanswered++;
+      trivia.result = false;
       clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+    }
+    // if all the questions have been shown end the game, show results
+    else if(trivia.currentSet === Object.keys(trivia.questions).length){
       
-      // show game section
+      // adds results of game (correct, incorrect, unanswered) to the page
+      $('#results')
+        .html('<h3>Thank you for playing!</h3>'+
+        '<p>Correct: '+ trivia.correct +'</p>'+
+        '<p>Incorrect: '+ trivia.incorrect +'</p>'+
+        '<p>Unaswered: '+ trivia.unanswered +'</p>'+
+        '<p>Please play again!</p>');
       
-      // show timer
+      // hide game sction
+      $('#game').hide();
       
-    // method to loop through and display questions and options 
- 
-      // set timer 
-      
-      // to prevent timer speed up
-
-      
-      // gets all the questions 
+      // show start button to begin a new game
+      $('#start').show();
+    }
     
-      
-    // method to decrement counter and count unanswered if timer runs out
+  },
+  // method to evaluate the option clicked
+  guessChecker : function() {
     
-      // if timer still has time left and there are still questions left to ask
+    // timer ID for gameResult setTimeout
+    var resultId;
+    
+    // the answer to the current question being asked
+    var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+    
+    // if the text of the option picked matches the answer of the current question, increment correct
+    if($(this).text() === currentAnswer){
+      // turn button green for correct
+      $(this).addClass('btn-success').removeClass('btn-info');
+      
+      trivia.correct++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Correct Answer!</h3>');
+    }
+    // else the user picked the wrong option, increment incorrect
+    else{
+      // turn button clicked red for incorrect
+      $(this).addClass('btn-danger').removeClass('btn-info');
+      
+      trivia.incorrect++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
+    }
+    
+  },
+  // method to remove previous question results and options
+  guessResult : function(){
+    
+    // increment to next question set
+    trivia.currentSet++;
+    
+    // remove the options and results
+    $('.option').remove();
+    $('#results h3').remove();
+    
+    // begin next question
+    trivia.nextQuestion();
      
-      // the time has run out and increment unanswered, run result
-      
-      // if all the questions have been shown end the game, show results
-     
-        // adds results of game to the page
-      
-      
-      // timer ID for gameResult setTimeout
- 
-      // the answer to the current question being asked
-      
-      // if the text of the option picked matches the answer of the current question, increment correct
-
-    
-      // else the user picked the wrong option, increment incorrect
-  
   }
+
+}
